e9bf7921bf4122a48fc87a22a882a83ff44c6e94
Eduardo Navarro Poveda <edunavarro13@gmail.com>
2019-03-05 23:32:07 +0100

    Terminado DestroyStones-IV-

---
+++ DestroyStones/Assets/Scripts/Stone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    // Altura a la que la piedra se destruira
+    private const float yDie = -30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < yDie)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
